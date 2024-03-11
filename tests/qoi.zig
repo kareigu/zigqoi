7ac15daabc59b6356741cb767d362ac8bb0484e9
karei <eetu.kivelae@gmail.com>
2024-03-11 20:13:11 +0200

    Update struct names in test cases

--- tests/qoi.zig
+++ tests/qoi.zig
@@ -7,7 +7,7 @@ const alloc = std.testing.allocator;
 const test_file = @embedFile("4x4.qoi");
 const png_file = @embedFile("4x4.png");
 
-test "read qoi_header" {
+test "read QoiHeader" {
     var header = try zigqoi.QoiHeader.from_bytes(test_file);
     try std.testing.expect(std.mem.eql(u8, &header.magic.str(), zigqoi.QoiHeader.magic_string.valid_str));
     try std.testing.expect(header.width == 4);
@@ -18,7 +18,7 @@ test "read qoi_header" {
     try std.testing.expectError(zigqoi.QoiHeader.header_error.WrongFiletype, zigqoi.QoiHeader.from_bytes(png_file));
 }
 
-test "read qoi_image" {
+test "read QoiImage" {
     var image = try zigqoi.QoiImage.from_bytes(alloc, test_file);
     defer image.free(alloc);
     try std.testing.expect(image.pixels.len == image.header.width * image.header.height);
