0f5786e6a441ac5903c05987703a3fb537b0ba5a
karei <eetu.kivelae@gmail.com>
2024-03-11 19:51:58 +0200

    Add other `QOI_OP` enum values

--- src/qoi.zig
+++ src/qoi.zig
@@ -81,6 +81,11 @@ const pixel = packed struct {
 
 const QOI_OP = enum(u8) {
     QOI_OP_RGB = 0b11111110,
+    QOI_OP_RGBA = 0b11111111,
+    QOI_OP_INDEX = 0b00000000,
+    QOI_OP_DIFF = 0b01000000,
+    QOI_OP_LUMA = 0b10000000,
+    QOI_OP_RUN = 0b11000000,
 };
 
 pub const qoi_image = struct {
