99a67e1c43d2d67578428f3490b84b13eebbf5b7
karei <eetu.kivelae@gmail.com>
2024-03-13 19:15:39 +0200

    Update tests

--- tests/qoi.zig
+++ tests/qoi.zig
@@ -4,12 +4,11 @@ const zigqoi = @import("zigqoi");
 
 const alloc = std.testing.allocator;
 
-const test_file = @embedFile("4x4.qoi");
+const four_by_four = @embedFile("4x4.qoi");
 const png_file = @embedFile("4x4.png");
 
 test "read QoiHeader" {
-    var header = try zigqoi.QoiHeader.from_bytes(test_file);
-    try std.testing.expect(std.mem.eql(u8, &header.magic.str(), zigqoi.QoiHeader.magic_string.valid_str));
+    var header = try zigqoi.QoiHeader.from_bytes(four_by_four);
     try std.testing.expect(header.width == 4);
     try std.testing.expect(header.height == 4);
     try std.testing.expect(header.channels == 3);
@@ -19,7 +18,7 @@ test "read QoiHeader" {
 }
 
 test "read QoiImage" {
-    var image = try zigqoi.QoiImage.from_bytes(alloc, test_file);
+    var image = try zigqoi.QoiImage.from_bytes(alloc, four_by_four);
     defer image.free(alloc);
     try std.testing.expect(image.pixels.len == image.header.width * image.header.height);
     try std.testing.expectError(zigqoi.QoiHeader.HeaderError.WrongFiletype, zigqoi.QoiImage.from_bytes(alloc, png_file));
@@ -27,5 +26,5 @@ test "read QoiImage" {
 
 test "read QoiImage OutOfMemory" {
     const fail_alloc = std.testing.failing_allocator;
-    try std.testing.expectError(zigqoi.QoiImage.QoiError.OutOfMemory, zigqoi.QoiImage.from_bytes(fail_alloc, test_file));
+    try std.testing.expectError(zigqoi.QoiImage.QoiError.OutOfMemory, zigqoi.QoiImage.from_bytes(fail_alloc, four_by_four));
 }
