36f918fb05cd61fa9115d0aecf5e457d8ea4148b
karei <eetu.kivelae@gmail.com>
2024-03-15 21:03:53 +0200

    Implement testing command to validate encoding/decoding
    Allows reading a qoi file from a path, decodes and prints the image once,
    then encodes the image again and finally, decodes the encoded image and prints it.
    This allows for easily checking byte/visual discrepencies in the produced files

--- tests/main.zig
+++ tests/main.zig
@@ -4,6 +4,7 @@ const zigqoi = @import("zigqoi");
 const Command = enum {
     Encode,
     Decode,
+    Test,
     Invalid,
 };
 
@@ -41,12 +42,19 @@ pub fn main() !void {
                 continue;
             }
         }
+        if (std.mem.startsWith(u8, arg, "t")) {
+            if (arg.len == 1 or std.mem.eql(u8, arg, "test")) {
+                command = .Test;
+                continue;
+            }
+        }
         filepath = arg;
     }
 
     switch (command) {
         .Decode => try decode(alloc, filepath, enable_hex_print),
         .Encode => try encode(alloc, filepath),
+        .Test => try test_decode_encode(alloc, filepath),
         .Invalid => {
             std.debug.print("Select a command\n", .{});
             std.process.exit(1);
@@ -131,4 +139,79 @@ fn encode(alloc: std.mem.Allocator, filepath: []const u8) !void {
     }
     try buf_writer.flush();
     std.debug.print("\n", .{});
+}
+
+fn test_decode_encode(alloc: std.mem.Allocator, filepath: []const u8) !void {
+    const file = try std.fs.cwd().openFile(filepath, .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var stream = buf_reader.reader();
+
+    const metadata = try file.metadata();
+    var buffer = try alloc.alloc(u8, metadata.size());
+    const bytes_read = try stream.readAll(buffer);
+    std.debug.print("read {} bytes\n", .{bytes_read});
+
+    var image = zigqoi.QoiImage.from_bytes(alloc, buffer) catch |e| {
+        std.debug.print("ERROR: {s}\n", .{@errorName(e)});
+        std.process.exit(1);
+    };
+    defer image.free(alloc);
+    std.debug.print("header for '{s}':\n", .{filepath});
+    std.debug.print("  width: {}\n", .{image.header.width});
+    std.debug.print("  height: {}\n", .{image.header.height});
+    std.debug.print("  channels: {}\n", .{image.header.channels});
+    std.debug.print("  colour_space: {}\n", .{image.header.colour_space});
+
+    std.debug.print("pixels:\n", .{});
+    {
+        var x: u32 = 0;
+        for (image.pixels) |pixel| {
+            std.debug.print("\x1b[48;2;{};{};{}m", .{ pixel.r, pixel.g, pixel.b });
+            std.debug.print("  ", .{});
+            std.debug.print("\x1b[0m", .{});
+            x += 1;
+            if (x >= image.header.width) {
+                std.debug.print("\n", .{});
+                x = 0;
+            }
+        }
+        std.debug.print("\n", .{});
+    }
+
+    const bytes = try image.to_bytes(alloc);
+    std.debug.print("encoded ({} bytes): ", .{bytes.len});
+    for (bytes) |byte| {
+        std.debug.print("{x:0>2}", .{byte});
+    }
+    std.debug.print("\n", .{});
+
+    image = zigqoi.QoiImage.from_bytes(alloc, bytes) catch |e| {
+        std.debug.print("ERROR: {s}\n", .{@errorName(e)});
+        std.process.exit(1);
+    };
+    defer image.free(alloc);
+    std.debug.print("header for '{s}':\n", .{filepath});
+    std.debug.print("  width: {}\n", .{image.header.width});
+    std.debug.print("  height: {}\n", .{image.header.height});
+    std.debug.print("  channels: {}\n", .{image.header.channels});
+    std.debug.print("  colour_space: {}\n", .{image.header.colour_space});
+
+    std.debug.print("pixels:\n", .{});
+
+    {
+        var x: u32 = 0;
+        for (image.pixels) |pixel| {
+            std.debug.print("\x1b[48;2;{};{};{}m", .{ pixel.r, pixel.g, pixel.b });
+            std.debug.print("  ", .{});
+            std.debug.print("\x1b[0m", .{});
+            x += 1;
+            if (x >= image.header.width) {
+                std.debug.print("\n", .{});
+                x = 0;
+            }
+        }
+        std.debug.print("\n", .{});
+    }
 }
